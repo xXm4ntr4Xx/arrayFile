98d693dff7d89a5e8a9d131d50a7e780cf1e2325
xXm4ntr4Xx <pietroannobil@gmail.com>
2021-11-18 16:02:53 +0000

    main.js
    this is the main js file text

---
+++ main.js
@@ -0,0 +1,37 @@
+const favoriteFood = ["cheese","chocolate","olives","anchovies"];
+const favoriteSongs = ["Tiny Dancer","Running Up That Hill","Don't Stop Moving","Tragedy","Crazy Horses"];
+const pyramidScheme = [1,2,4,8,16,32,64];
+const team = [["Ben","rapping"],["Tao","hand raising"],["Lizzie","being friendly"],["Liz","crochet"],["Patrick","Call of Duty"],["Chris","changing lives"],["Tim","analogies"],["James","friendship"],["Joseph","being positive"],["Max","papier-mache"]];
+
+let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  //console.log(months[3])
+
+  let englishNumbers = ["one", "two", "three", "four", "five","six","seven","eigth"];
+
+/* 
+let i = 0;
+
+  while(i <= 2){
+      console.log(englishNumbers[i]);
+      index++
+      }
+*/
+
+let words = ["ben", "ha", "spla", "fa", "ca", "dra"];
+  
+for (let i = 0; i < words.length; i++) {
+    console.log(words[i]);
+}
